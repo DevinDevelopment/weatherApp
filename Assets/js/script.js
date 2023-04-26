f7ce358a7f09370775110a8410a829cfd8858351
Devin Nunez <dnunez@gibbswire.com>
2023-04-25 23:18:02 -0400

    added more fields

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,6 +4,10 @@ var searchButton = document.querySelector(".searchButton");
 var weatherList = document.querySelector(".listGroup");
 
 var cityTitle = document.querySelector(".subtitle");
+var listItem1 = document.querySelector(".listItem1");
+var listItem2 = document.querySelector(".listItem2");
+var listItem3 = document.querySelector(".listItem3");
+
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -35,6 +39,9 @@ function getAPI(city) {
         console.log(data);
         console.log(data.city.name);
         cityTitle.textContent = data.city.name;
+        listItem1.textContent = ("Temp: " + data.list[0].main.temp);
+        listItem2.textContent = ("humidity: " + data.list[0].main.humidity);
+
     });
 }
 
